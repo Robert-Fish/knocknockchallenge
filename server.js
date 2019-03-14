a74ca866789bad774bd037d22bfd9e72e723d03a
Robert-Fish <robert@inflowapp.com.au>
2019-03-14 15:31:51 +1000

    added triangle type

--- server.js
+++ server.js
@@ -24,8 +24,31 @@ app.get("/api/ReverseWords", (req, res) => {
     return result;
   };
 
-  // res.send(reverseWord(req.params.sentence));
-  res.send("hello world");
+  res.send(reverseWord(req.query.sentence));
 });
 
-app.listen(8000, () => console.log("server listening on port 8000"));
+app.get("/api/TriangleType", (req, res) => {
+  const { a, b, c } = req.query; // Simple destructuring to make the code cleaner
+
+  getTriangleType = (a, b, c) => {
+    return (
+      (a === b && b == c && "equilateral") ||
+      ((a === b || a === c || b === c) && "isosceles") ||
+      "scalene"
+    );
+  };
+
+  // Checks all three variables against set cases to return triangle type
+
+  res.send(getTriangleType(a, b, c));
+});
+
+app.get("/api/Token", (req, res) => {
+  // Checks all three variables against set cases to return triangle type
+
+  res.send("00000000-0000-0000-0000-000000000000");
+});
+
+const PORT = process.env.PORT || 8000; // Dynamic port for deployment
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
